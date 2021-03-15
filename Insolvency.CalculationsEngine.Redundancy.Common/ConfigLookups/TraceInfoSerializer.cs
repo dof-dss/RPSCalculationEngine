cc65896c4e87157aff4dea3b2069bf89f8b063ab
Govind <td@gmail.com>
2021-03-15 13:22:22 +0000

    ASP-86 - TraceInfo added temporary json data

---
+++ Insolvency.CalculationsEngine.Redundancy.Common/ConfigLookups/TraceInfoSerializer.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Insolvency.CalculationsEngine.Redundancy.Common.ConfigLookups
+{
+    public static class TraceInfoSerializer
+    {
+        public static string GetTraceDetails()
+        {
+            return JsonConvert.SerializeObject(new TraceInfo());
+        }
+    }
+
+    public class TraceInfo
+    {
+        public DateTime DateTime { get; set; } = DateTime.Now;
+    }
+
+}
